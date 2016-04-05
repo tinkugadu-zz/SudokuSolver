20bf3b1e02df7fb379b755f74539e36c78da1847
tinkugadu <araveti.venkata.sudeepth@gmail.com>
2016-04-05 18:38:08 -0400

    Modified Sudoku class for proper data structure.

--- SudokuClass.h
+++ SudokuClass.h
@@ -1,15 +1,14 @@
 #ifndef SUDOKU_CLASS_H
 #define SUDOKU_CLASS_H
 
-#define SUDOKU_SIZE 9
+typedef std::vector<int> Row;
+typedef std::vector<Row> Puzzle;
 class Sudoku
 {
-    void getArr(int *tmpArr);
-    void setArr(int *tmpArr);
-    void setArr(int i, int j, int val);
-    bool isCompleted();
-    void printPuzzle();
-private:
-    int _arr[SUDOKU_SIZE][SUDOKU_SIZE];
+    Sudoku();
+    ~Sudoku();
+    Sudoku(Sudoku &cpy) = delete;
+public:
+    Puzzle _puzzle;
 };
 #endif

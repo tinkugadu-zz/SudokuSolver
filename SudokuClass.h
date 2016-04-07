38cdb89c242aeaa23ae663f06229caa318810c69
tinkugadu <araveti.venkata.sudeepth@gmail.com>
2016-04-06 21:32:35 -0400

    restructure data objects in sudoku class

--- SudokuClass.h
+++ SudokuClass.h
@@ -1,14 +1,24 @@
 #ifndef SUDOKU_CLASS_H
 #define SUDOKU_CLASS_H
 
+#include <map>
+
 typedef std::vector<int> Row;
 typedef std::vector<Row> Puzzle;
+typedef std::map<int, Row> PROBS;
+
 class Sudoku
 {
-    Sudoku();
-    ~Sudoku();
     Sudoku(Sudoku &cpy) = delete;
+    PROBS _possibValues;
+    int **_puzzle;
 public:
-    Puzzle _puzzle;
+    Sudoku(int siz, int ssiz);
+    ~Sudoku();
+    //setters and getters
+    bool setRow(int row, int *arr);
+    bool getRow(int row, int *arr) const;
+    bool setVal(int row, int col, int &val);
+    bool getVal(int row, int col, int &val) const;
 };
 #endif

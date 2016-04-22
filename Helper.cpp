82ad12a0bba8a54192a8dad71c669cfdf42ed495
tinkugadu <araveti.venkata.sudeepth@gmail.com>
2016-04-21 20:23:40 -0400

    helper functions for vectors and map

---
+++ Helper.cpp
@@ -0,0 +1,71 @@
+#include "utils.h"
+
+void InsertOrder(Row &tmpRow, int val)
+{
+    Row::iterator it = tmpRow.begin();
+    bool inserted = false;
+    while(it != tmpRow.end())
+    {
+        if(val >= *it)
+        {
+            ++it;
+            continue;
+        }
+        else
+        {
+            tmpRow.insert(it, val);
+            inserted = true;
+            break;
+        }
+    }
+    if(!inserted)
+    {
+        tmpRow.push_back(val);
+    }
+}
+
+Row::iterator Find(Row &tmpRow, int val)
+{
+    int siz = tmpRow.size() - 1;
+    int left_ind = 0;
+    int right_ind = siz;
+    int ind = 0;
+    bool found = false;
+    do
+    {
+        ind = left_ind + (right_ind - left_ind)/2;
+        if(tmpRow[ind] == val) 
+        {
+            found = true;
+            break;
+        }
+        if(val > tmpRow[ind])
+        {
+        //shift right to the region
+            left_ind = ind;
+        }
+        if(val < tmpRow[ind])
+        {
+	 //shift left to region
+            right_ind = ind;
+        }
+    }
+    while(right_ind - left_ind > 1);
+    if(tmpRow[left_ind] == val)
+    {
+        ind = left_ind;
+        found = true;
+    }
+    if(tmpRow[right_ind] == val)
+    {
+        ind = right_ind;
+        found = true;
+    }
+    
+    if(found)
+    {
+        cout<<"index found at: "<<ind<<endl;
+        return tmpRow.begin() + ind;
+    }
+    else return tmpRow.end();
+}
